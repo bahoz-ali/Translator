42a447e12038155eca8a1bafa38858896bd84130
Bahoz <bahozali50@gmail.com>
2022-08-17 18:10:15 +0300

    add cash for storing the kurdish result and english text, so if user send same english text, we do not send new request, we normally send the user kurdish cashed result

--- dic.js
+++ dic.js
@@ -2,25 +2,33 @@ const translation = document.querySelector('.translation');
 const textToBeTranslate = document.querySelector('#text');
 const translateBtn = document.querySelector('#translate-btn');
 
+let cashEnglishText = '';
+let cashKurdishText = '';
+
 async function translate2(value) {
   let url = 'https://microsoft-translator-text.p.rapidapi.com/translate';
   url += `?to[0]=ku&api-version=3.0&profanityAction=noAction&textType=plain`;
 
+  if (cashEnglishText == value) return cashKurdishText;
+  
   const options = {
     method: 'POST',
     headers: {
-    'content-type': 'application/json',
-    'X-RapidAPI-Key': '6aa4aeabfcmshd46ef8b482c1622p1541b2jsne7988d0e92d6',
-    'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
-  },
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': '6aa4aeabfcmshd46ef8b482c1622p1541b2jsne7988d0e92d6',
+      'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com',
+    },
     body: JSON.stringify([{ Text: value }]),
   };
+
+  cashEnglishText = value;
 
   const response = await fetch(url, options);
   const data = await response.json();
   // console.log(data);
 
   const result = data[0]?.translations[0]?.text;
+  cashKurdishText = result;
 
   if (!result || typeof result == 'undefined')
     return 'ببورە بەڕێز مانگانە ڕیگەم پێدراوە تەنیا ٣٠٠ لاپەڕە بکرێتەوە کوردی بۆیە تا مانگێکی تر ناتوانیی هیچ بکەیتەوە بە کوردی، یان دەێت من مانگانە ٦٠ دۆلاریان پێبدەم بۆئەوەی وەرگێڕەکە ڕانەگرن';
@@ -33,7 +41,7 @@ translateBtn.addEventListener('click', async () => {
 
   console.log(text);
   const result = await translate2(text);
-  console.log('text: ', result);
+
   translation.innerHTML = result;
 });
 
